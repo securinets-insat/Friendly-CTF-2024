ca7608055372c2fe2764bb547948190f7910a577
Jihed Kdiss <jihedkdiss@outlook.com>
2024-09-05 03:43:55 +0100

    2 more conditions

--- Reverse/Serial Key/serialkey.c
+++ Reverse/Serial Key/serialkey.c
@@ -3,20 +3,34 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main() {
+int prime(int n)
+{
+    if (n <= 1)
+        return 0;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main()
+{
     printf("Enter serial key: ");
     char serial_key[123];
     scanf("%123s", serial_key);
 
     int condition1 = 0;
-    while(serial_key[condition1] != '\0') {
+    while (serial_key[condition1] != '\0')
+    {
         condition1++;
     }
 
     int i = 0;
     int condition2 = 1;
     while (i < condition1)
-    {   
+    {
         if (i % 2 == 0)
         {
             if ((serial_key[i] - '0') % 2 != 0)
@@ -37,10 +51,12 @@ int main() {
     }
 
     int condition3 = 1;
-    for (i = 0; i <= condition1 - 6; i += 3) {
-        int sum1 = (serial_key[i] - '0') + (serial_key[i+1] - '0') + (serial_key[i+2] - '0');
-        int sum2 = (serial_key[i+3] - '0') + (serial_key[i+4] - '0') + (serial_key[i+5] - '0');
-        if (sum1 <= sum2) {
+    for (i = 0; i <= condition1 - 6; i += 3)
+    {
+        int sum1 = (serial_key[i] - '0') + (serial_key[i + 1] - '0') + (serial_key[i + 2] - '0');
+        int sum2 = (serial_key[i + 3] - '0') + (serial_key[i + 4] - '0') + (serial_key[i + 5] - '0');
+        if (sum1 <= sum2)
+        {
             condition3 = 0;
             break;
         }
@@ -48,22 +64,27 @@ int main() {
 
     int condition4 = 0;
     int sum_all_digits = 0;
-    for (i = 0; i < condition1; i++) {
+    for (i = 0; i < condition1; i++)
+    {
         sum_all_digits += (serial_key[i] - '0');
     }
 
-    if (sum_all_digits == 159) {
+    if (sum_all_digits == 163)
+    {
         condition4 = 1;
     }
 
     int condition5 = 0;
-    if (strncmp(serial_key, "89898", 5) == 0 && strncmp(&serial_key[condition1-5], "12125", 5) == 0) {
+    if (strncmp(serial_key, "89898", 5) == 0 && strncmp(&serial_key[condition1 - 5], "12125", 5) == 0)
+    {
         condition5 = 1;
     }
 
     int condition6 = 1;
-    for (i = 0; i < condition1; i++) {
-        if (serial_key[i] == '0') {
+    for (i = 0; i < condition1; i++)
+    {
+        if (serial_key[i] == '0')
+        {
             condition6 = 0;
             break;
         }
@@ -78,16 +99,42 @@ int main() {
 
     int fourth_group_number = atoi(fourth_group);
 
-    if (sqrt(fourth_group_number) == 27) {
+    if (sqrt(fourth_group_number) == 27)
+    {
         condition7 = 1;
     }
 
-    int all_conditions_true = (condition1 == 30) && condition2 && condition3 && condition4 && condition5 && condition6 && condition7;
+    int condition8 = 0;
+    char whatever_group[4];
+    whatever_group[0] = serial_key[21];
+    whatever_group[1] = serial_key[22];
+    whatever_group[2] = serial_key[23];
+    whatever_group[3] = '\0';
 
-    if(all_conditions_true) {
+    int whatever_group_number = atoi(whatever_group);
+
+    if (prime(whatever_group_number))
+    {
+        condition8 = 1;
+    }
+
+    char third_group[4];
+    third_group[0] = serial_key[6];
+    third_group[1] = serial_key[7];
+    third_group[2] = serial_key[8];
+    third_group[3] = '\0';
+
+    int condition9 = strncmp(third_group, "698", 3) == 0;
+
+    int all_conditions_true = (condition1 == 30) && condition2 && condition3 && condition4 && condition5 && condition6 && condition7 && condition8 && condition9;
+
+    if (all_conditions_true)
+    {
         printf("Securinets{%s}\n", serial_key);
         return 0;
-    } else {
+    }
+    else
+    {
         printf("Try again.\n");
         return 1;
     }
