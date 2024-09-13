bb427d93b915ec35cf3b034cba07f56f9e78c96f
wassimjhinaoui <wassajh@gmail.com>
2024-09-13 15:45:47 -0400

    added devtools challange

---
+++ Web/DevTools_for_the_win/src/app.js
@@ -0,0 +1,17 @@
+
+
+
+var hackerName = prompt("what is your name Mr. Hacker","anonymous");
+h1 = document.getElementsByTagName("h1")[0] ;
+
+if (hackerName == "anonymous") {
+    h1.innerHTML = `Hello ${hackerName} <br> good job a hacker never gives his name`
+}else{
+    h1.innerHTML = `Hello ${hackerName} <br> be careful , a hacker never gives his name`
+    
+}
+
+
+
+
+/* hello there, very nice styiling don't you think :) */
