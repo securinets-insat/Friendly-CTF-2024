3bb085a1ede4697452a280b603089ee6fad00f95
wassimjhinaoui <wassajh@gmail.com>
2024-10-01 18:19:48 -0400

    barcha hajet tzedou

--- Web/down.sh
+++ Web/down.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-directories=$(find . -type d)
-if [[ -z "$@" ]] ; then
-	for directory in $directories ; do
-		echo "starting $directory"
-		docker-compose -f "$directory"/compose.yaml down
-	done
-else 
-	for directory in "$@" ; do
-		if test -d "./$directory" ; then
-			docker-compose -f ./"$directory"/compose.yaml down 
-		else 
-			echo "$directory doesnt exist"
-		fi
-	done
+stop_container() {
+    local directory="$1"
+    echo "Stopping $directory"
+    if [ -f "$directory/compose.yaml" ]; then
+        docker-compose -f "$directory/compose.yaml" down
+    elif [ -f "$directory/docker-compose.yaml" ]; then
+        docker-compose -f "$directory/docker-compose.yaml" down
+    else
+        echo "No compose file found in $directory"
+    fi
+}
+
+if [[ $# -eq 0 ]] ; then
+    for directory in */; do
+        directory="${directory%/}"  # Remove trailing slash
+        if [ "$directory" != "." ] && [ "$directory" != ".." ]; then
+            stop_container "$directory"
+        fi
+    done
+else
+    for directory in "$@"; do
+        if [ -d "./$directory" ]; then
+            stop_container "./$directory"
+        else
+            echo "$directory doesn't exist"
+        fi
+    done
 fi
--- Web/up.sh
+++ Web/up.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-directories=$(find . -type d)
-if [[ -z "$@" ]] ; then
-	for directory in $directories ; do
-		echo "starting $directory"
-		docker-compose -f "$directory"/compose.yaml up --build -d
-	done
-else 
-	for directory in "$@" ; do
-		if test -d "./$directory" ; then
-			docker-compose -f ./"$directory"/compose.yaml up --build -d 
-		else 
-			echo "$directory doesnt exist"
-		fi
-	done
+start_container() {
+    local directory="$1"
+    echo "Starting $directory"
+    if [ -f "$directory/compose.yaml" ]; then
+        docker-compose -f "$directory/compose.yaml" up --build -d
+    elif [ -f "$directory/docker-compose.yaml" ]; then
+        docker-compose -f "$directory/docker-compose.yaml" up --build -d
+    else
+        echo "No compose file found in $directory"
+    fi
+}
+
+if [[ $# -eq 0 ]] ; then
+    for directory in */; do
+        directory="${directory%/}"  # Remove trailing slash
+        if [ "$directory" != "." ] && [ "$directory" != ".." ]; then
+            start_container "$directory"
+        fi
+    done
+else
+    for directory in "$@"; do
+        if [ -d "./$directory" ]; then
+            start_container "./$directory"
+        else
+            echo "$directory doesn't exist"
+        fi
+    done
 fi
